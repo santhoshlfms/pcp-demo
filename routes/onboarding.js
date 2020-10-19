9df1c17efc88b016249a490a27449e705108180c
T A <arvta@paypal.com>
2020-10-19 19:32:50 +0530

    onboarding link

--- routes/onboarding.js
+++ routes/onboarding.js
@@ -1,5 +1,5 @@
 module.exports = function (router) {
-  // router.get(["/onboarding/first-party"], function (req, res, next) {
-  //   res.render("onboarding/first-party");
-  // });
+  router.get(["/onboarding/first-party"], function (req, res, next) {
+    res.render("onboarding/first-party");
+  });
 };
